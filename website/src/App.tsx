d9d2f610a7bbbd6732b34b7f81807b41cc712f34
batuhan.yapanoglu <batuhan.yapanoglu@comeon.com>
2021-08-10 11:54:43 +0300

    Add eslint & fix some linting errors and styling

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import GameList from "./GameList";
-import Container from "@material-ui/core/Container";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import "./App.css";
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import GameList from './GameList';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Container className="conatiner" maxWidth="sm">
+      <Container className="container" maxWidth="sm">
         <Card>
           <CardContent>
             <h3>List of Games</h3>
--- website/src/GameCell.tsx
+++ website/src/GameCell.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Switch from "@material-ui/core/Switch";
-import IconButton from "@material-ui/core/IconButton";
-import RightIcon from "@material-ui/icons/KeyboardArrowRight";
-import "./Game.css";
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import RightIcon from '@material-ui/icons/KeyboardArrowRight';
+import './Game.css';
 
 export interface Game {
   id: string;
@@ -13,26 +12,30 @@ export interface Game {
   code: string;
 }
 
-
 function GameCell(props: any) {
   const { game } = props;
-
-  console.log("Current game : ", game)
   return (
     <div className="Game">
       <img src={game.icon} alt="game-thumbnail" />
-      <p><b>{game.name}</b>
-        {game.short_desc}</p>
-      <IconButton aria-label="play" onClick={e => {
-        console.log("Go to Details page of", game.code);
-        let assertedWindow = window as any
-        // Trigger bridge message for IOS (What will be the equivalent for Android?)
-        if (assertedWindow.webkit && assertedWindow.webkit.messageHandlers && assertedWindow.webkit.messageHandlers.toggleMessageHandler) {
-          assertedWindow.webkit.messageHandlers.toggleMessageHandler.postMessage({
-            "gameChosen": game.code
-          });
-        }
-      }}>
+      <p>
+        <b>{game.name}</b>
+        {game.short_desc}
+      </p>
+      <IconButton
+        aria-label="play"
+        onClick={() => {
+          const assertedWindow = window as any;
+          // Trigger bridge message for IOS (What will be the equivalent for Android?)
+          if (assertedWindow.webkit
+              && assertedWindow.webkit.messageHandlers
+              && assertedWindow.webkit.messageHandlers.toggleMessageHandler
+          ) {
+            assertedWindow.webkit.messageHandlers.toggleMessageHandler.postMessage({
+              gameChosen: game.code,
+            });
+          }
+        }}
+      >
         <RightIcon fontSize="large" />
       </IconButton>
     </div>
